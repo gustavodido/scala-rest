f04ba48717e792a2b212e4c310863a1abd8f85aa
Gu1f3f3 <gustavo_di_domenico@gap.com>
2018-04-12 21:05:20 -0700

    Add greetings controller.

--- src/main/scala/test/Application.scala
+++ src/main/scala/test/Application.scala
@@ -1,3 +1,5 @@
+package test
+
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
---
+++ src/main/scala/test/controllers/HelloController.scala
@@ -0,0 +1,10 @@
+package test.controllers
+
+import org.springframework.web.bind.annotation.{RequestMapping, RestController}
+
+@RestController
+class HelloController {
+
+  @RequestMapping(Array("/"))
+  def index() = "Greetings from Spring Boot!"
+}
