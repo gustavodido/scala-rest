c1bc88a11e724e2fa4ea72c35ac34227fdf01ce5
Gu1f3f3 <gustavo_di_domenico@gap.com>
2018-04-12 21:08:26 -0700

    Add test support.

---
+++ src/test/scala/test/controllers/HelloControllerTest.scala
@@ -0,0 +1,9 @@
+package test.controllers
+
+import org.scalatest.FlatSpec
+
+class HelloControllerTest extends FlatSpec {
+  "Hello controller" should "say nice greetings" in {
+    assert(true == true)
+  }
+}
