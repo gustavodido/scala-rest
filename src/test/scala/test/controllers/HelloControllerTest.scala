d4df8bc08109d23a2704a85614c7511d3e371678
Gu1f3f3 <gustavo_di_domenico@gap.com>
2018-04-12 21:25:07 -0700

    Add mock mvc test.

--- src/test/scala/test/controllers/HelloControllerTest.scala
+++ src/test/scala/test/controllers/HelloControllerTest.scala
@@ -1,9 +1,35 @@
 package test.controllers
 
-import org.scalatest.FlatSpec
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.scalatest.{BeforeAndAfter, FlatSpec}
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+import org.springframework.test.context.web.WebAppConfiguration
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.context.WebApplicationContext
 
-class HelloControllerTest extends FlatSpec {
-  "Hello controller" should "say nice greetings" in {
-    assert(true == true)
+@RunWith(classOf[SpringJUnit4ClassRunner])
+@WebAppConfiguration
+@Test
+class HelloControllerTest extends FlatSpec with BeforeAndAfter {
+
+  @Autowired
+  val webApplicationContext: WebApplicationContext = null
+  var mockMvc: MockMvc = _
+
+  before {
+    mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build()
+  }
+
+  "Hello Controller" should "give me greetings" in {
+    val result = mockMvc.perform(get("/test.html"))
+      .andExpect(status().isOk())
+      .andReturn()
+
+    assert(result.getResponse.getContentAsString == "Greetings from Spring Boot!")
   }
 }
